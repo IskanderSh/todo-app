9f0ddba6e4781d133300bc0d0a9c9ae784ca7927
IskanderSh <is2367361@gmail.com>
2023-06-19 01:02:42 +0300

    cover getUser func of user with tests

--- pkg/repository/auth_postgres_test.go
+++ pkg/repository/auth_postgres_test.go
@@ -71,3 +71,74 @@ func TestAuthPostgres_CreateUser(t *testing.T) {
 		})
 	}
 }
+
+func TestAuthPostgres_GetUser(t *testing.T) {
+	db, mock, err := sqlmock.Newx()
+	if err != nil {
+		log.Fatalf("error on testing TestAuthPostgres_GetUser func: %v", err)
+	}
+	defer db.Close()
+
+	r := NewAuthPostgres(db)
+
+	type args struct {
+		username string
+		password string
+	}
+
+	testTable := []struct {
+		name         string
+		args         args
+		mockBehavior func(args args)
+		want         todo.User
+		wantErr      bool
+	}{
+		{
+			name: "OK",
+			args: args{
+				username: "test",
+				password: "qwerty",
+			},
+			mockBehavior: func(args args) {
+				rows := sqlmock.NewRows([]string{"id", "name", "username", "password"}).
+					AddRow("1", "Test", "test", "qwerty")
+				mock.ExpectQuery("SELECT (.+) FROM users WHERE (.+)").WithArgs(args.username, args.password).
+					WillReturnRows(rows)
+			},
+			want: todo.User{
+				Id:       1,
+				Name:     "Test",
+				Username: "test",
+				Password: "qwerty",
+			},
+		},
+		{
+			name: "Empty Fields",
+			args: args{
+				username: "",
+				password: "qwerty",
+			},
+			mockBehavior: func(args args) {
+				rows := sqlmock.NewRows([]string{"id", "name", "username", "password"})
+
+				mock.ExpectQuery("SELECT (.+) FROM users WHERE (.+)").WithArgs(args.username, args.password).
+					WillReturnRows(rows)
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			testCase.mockBehavior(testCase.args)
+
+			got, err := r.GetUser(testCase.args.username, testCase.args.password)
+			if testCase.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, testCase.want, got)
+			}
+		})
+	}
+}
