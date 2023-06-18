98c1773692adb389aacab9f576038822a21ff49e
IskanderSh <is2367361@gmail.com>
2023-06-18 21:35:56 +0300

    cover sign-in func with tests

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -34,13 +34,13 @@ func (h *Handler) signIn(c *gin.Context) {
 	var input signInInput
 
 	if err := c.BindJSON(&input); err != nil {
-		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		newErrorResponse(c, http.StatusBadRequest, "invalid input body")
 		return
 	}
 
 	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
 	if err != nil {
-		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		newErrorResponse(c, http.StatusUnauthorized, "incorrect login or password")
 		return
 	}
 
--- pkg/handler/auth_test.go
+++ pkg/handler/auth_test.go
@@ -90,3 +90,94 @@ func TestHandler_signUp(t *testing.T) {
 		})
 	}
 }
+
+func TestHandler_signIn(t *testing.T) {
+	type mockBehavior func(s *mock_service.MockAuthorization, input signInInput)
+
+	testTable := []struct {
+		name                string
+		inputBody           string
+		inputUser           signInInput
+		mockBehavior        mockBehavior
+		expectedStatusCode  int
+		expectedRequestBody string
+	}{
+		{
+			name:      "OK",
+			inputBody: `{"username":"test", "password":"qwerty"}`,
+			inputUser: signInInput{
+				Username: "test",
+				Password: "qwerty",
+			},
+			mockBehavior: func(s *mock_service.MockAuthorization, input signInInput) {
+				s.EXPECT().GenerateToken(input.Username, input.Password).Return("token", nil)
+			},
+			expectedStatusCode:  200,
+			expectedRequestBody: `{"token":"token"}`,
+		},
+		{
+			name:      "Incorrect password",
+			inputBody: `{"username":"test", "password":"qwertyu"}`,
+			inputUser: signInInput{
+				Username: "test",
+				Password: "qwertyu",
+			},
+			mockBehavior: func(s *mock_service.MockAuthorization, input signInInput) {
+				s.EXPECT().GenerateToken(input.Username, input.Password).
+					Return("", errors.New("incorrect login or password"))
+			},
+			expectedStatusCode:  401,
+			expectedRequestBody: `{"message":"incorrect login or password"}`,
+		},
+		{
+			name:                "Empty Fields",
+			inputBody:           `{"password":"qwerty"}`,
+			mockBehavior:        func(s *mock_service.MockAuthorization, input signInInput) {},
+			expectedStatusCode:  400,
+			expectedRequestBody: `{"message":"invalid input body"}`,
+		},
+		//{
+		//	name:      "Service Failure",
+		//	inputBody: `{"username":"test", "password":"qwerty"}`,
+		//	inputUser: signInInput{
+		//		Username: "test",
+		//		Password: "qwerty",
+		//	},
+		//	mockBehavior: func(s *mock_service.MockAuthorization, input signInInput) {
+		//		s.EXPECT().GenerateToken(input.Username, input.Password).
+		//			Return("", errors.New("service failure"))
+		//	},
+		//	expectedStatusCode:  500,
+		//	expectedRequestBody: `{"message":"service failure"}`,
+		//},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			// Init Deps
+			c := gomock.NewController(t)
+			defer c.Finish()
+
+			auth := mock_service.NewMockAuthorization(c)
+			testCase.mockBehavior(auth, testCase.inputUser)
+
+			services := &service.Service{Authorization: auth}
+			handler := NewHandler(services)
+
+			// Test Server
+			r := gin.New()
+			r.POST("/sign-in", handler.signIn)
+
+			// Test Request
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest("POST", "/sign-in", bytes.NewBufferString(testCase.inputBody))
+
+			// Perform Request
+			r.ServeHTTP(w, req)
+
+			// Assert
+			assert.Equal(t, w.Code, testCase.expectedStatusCode)
+			assert.Equal(t, w.Body.String(), testCase.expectedRequestBody)
+		})
+	}
+}
